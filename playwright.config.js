841024d41525065215dbf9a08d7b333aa2f7e0a9
bainscoder <manpreetsinghbains@clanap.com>
2024-02-15 11:48:04 +0530

    updated script

--- playwright.config.js
+++ playwright.config.js
@@ -1,150 +1,110 @@
-// // @ts-check
-// const { defineConfig, devices } = require('@playwright/test');
-// const path = require('path');
-// /**
-//  * Read environment variables from file.
-//  * https://github.com/motdotla/dotenv
-//  */
-// // require('dotenv').config();
-
-// /**
-//  * @see https://playwright.dev/docs/test-configuration
-//  */
-// module.exports = defineConfig({
-//   testDir: './t',
-//   /* Run tests in files in parallel */
-//   fullyParallel: true,
-//   /* Fail the build on CI if you accidentally left test.only in the source code. */
-//   forbidOnly: !!process.env.CI,
-//   /* Retry on CI only */
-//   retries: process.env.CI ? 0: 0,
-//   /* Opt out of parallel tests on CI. */
-//   workers: process.env.CI ? 1 : undefined,
-//   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-//   reporter: 'html',
-//   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-//   use: {
-//     /* Base URL to use in actions like `await page.goto('/')`. */
-//     // baseURL: 'http://127.0.0.1:3000',
-
-//     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-//     trace: 'on-first-retry',
-//     screenshot: "on",
-//     video: "on",
-//     snapshotPath: `example.spec.js-snapshots/get-started-link-chromium-[process.env.PLATFORM].png`
-//     // snapshotPath: './t/example.spec.js-snapshots',
-//     // snapshotPath: path.resolve('./t/snapshots')
-//   },
-
-//   /* Configure projects for major browsers */
-//   projects: [
-//     {
-//       name: 'chromium',
-//       use: { ...devices['Desktop Chrome']},
-//     },
-
-//     {
-//       name: 'iPad_View_Safari',
-//       use: { ...devices['iPad Pro']},
-//     },
-
-//     // {
-//     //   name: 'firefox',
-//     //   use: { ...devices['Desktop Firefox'] },
-//     // },
-
-//     // {
-//     //   name: 'webkit',
-//     //   use: { ...devices['Desktop Safari'] },
-//     // },
-
-//     /* Test against mobile viewports. */
-//     {
-//       name: 'Mobile Chrome',
-//       use: { ...devices['Pixel 5']},
-//     },
-//     // {
-//     //   name: 'Mobile Safari',
-//     //   use: { ...devices['iPhone 12'] },
-//     // },
-
-//     /* Test against branded browsers. */
-//     // {
-//     //   name: 'Microsoft Edge',
-//     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-//     // },
-//     // {
-//     //   name: 'Google Chrome',
-//     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-//     // },
-//   ],
-
-  
-
-//   /* Run your local dev server before starting the tests */
-//   // webServer: {
-//   //   command: 'npm run start',
-//   //   url: 'http://127.0.0.1:3000',
-//   //   reuseExistingServer: !process.env.CI,
-//   // },
-// });
-
-// // function determineSnapshotPath() {
-// //   const browserName = process.env.PLAYWRIGHT_BROWSER;
-// //   const platform = process.env.PLAYWRIGHT_PLATFORM;
-
-// //   if (browserName && platform) {
-// //     return `./t/snapshots/${browserName}-${platform}.png`;
-// //   }
-
-// //   return './t/snapshots/';
-// // }
-
-// // function getSnapshotPath() {
-// //   if (process.env.GITHUB_ACTIONS && process.env.GITHUB_WORKSPACE) {
-// //     return path.join(process.env.GITHUB_WORKSPACE, 'test-results', 'snapshots');
-// //   } else {
-// //     return path.join(__dirname, 't', 'snapshots');
-// //   }
-// // }
-
-
-
+// @ts-check
 const { defineConfig, devices } = require('@playwright/test');
 const path = require('path');
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
 
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
 module.exports = defineConfig({
   testDir: './t',
+  /* Run tests in files in parallel */
   fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 0 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  /* Retry on CI only */
+  retries: process.env.CI ? 0: 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: "on",
     video: "on",
     snapshotPath: './t/example.spec.js-snapshots',
-    // Define a custom snapshotNameProvider function
-    snapshotNameProvider: ({ test, browserName, viewport, isChromium, isWebKit, isFirefox, isMobile }) => {
-      // Generate a custom snapshot name without platform suffixes
-      return `${test.fullName.replace(/ /g, '-')}-${browserName}.png`;
-    }
+    // snapshotPath: path.resolve('./t/snapshots')
   },
+  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome']},
     },
+
     {
       name: 'iPad_View_Safari',
       use: { ...devices['iPad Pro']},
     },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    /* Test against mobile viewports. */
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5']},
-    }
-  ]
+    },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
+
+// function determineSnapshotPath() {
+//   const browserName = process.env.PLAYWRIGHT_BROWSER;
+//   const platform = process.env.PLAYWRIGHT_PLATFORM;
+
+//   if (browserName && platform) {
+//     return `./t/snapshots/${browserName}-${platform}.png`;
+//   }
+
+//   return './t/snapshots/';
+// }
+
+// function getSnapshotPath() {
+//   if (process.env.GITHUB_ACTIONS && process.env.GITHUB_WORKSPACE) {
+//     return path.join(process.env.GITHUB_WORKSPACE, 'test-results', 'snapshots');
+//   } else {
+//     return path.join(__dirname, 't', 'snapshots');
+//   }
+// }
+
 
 
--- t/example.spec.js
+++ t/example.spec.js
@@ -1,13 +1,6 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
@@ -16,6 +9,8 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Installation' })).toHaveScreenshot(`get-started-link-${process.env.PLATFORM}.png`);
+  await expect(page.getByRole('heading', { name: 'Installation' })).toHaveScreenshot(`get-started-link.png`);
 
 });
+
+
