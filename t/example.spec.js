41888c8a2af3f0303e8164cb85e15adbf1abc031
bainscoder <manpreetsinghbains@clanap.com>
2024-02-19 17:21:04 +0530

    updated action

--- t/example.spec.js
+++ t/example.spec.js
@@ -4,13 +4,13 @@ const { test, expect } = require('@playwright/test');
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
+  await page.waitForTimeout(2000);
+
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-  expect (await page.getByRole('heading', { name: 'Installation' }).screenshot()).toMatchSnapshot("search.png");
+  expect (await page.getByRole('heading', { name: 'Installation' }).screenshot()).toMatchSnapshot("search.png",{maxDiffPixelRatio: 0.2});
 
 });
-
-
