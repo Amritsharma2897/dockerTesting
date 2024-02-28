88e2ad08e9e81b770e193ed751efa42123f54226
bainscoder <manpreetsinghbains@clanap.com>
2024-02-28 16:46:40 +0530

    updated script

--- t/auth.setup.js
+++ t/auth.setup.js
@@ -1,43 +1,44 @@
 // tests/auth.setup.js
-const { test, expect } = require('@playwright/test');
+// const { test, expect } = require('@playwright/test');
+// test('authenticate', async ({ page }) => {
+//   // Navigate to Gmail login page
+//   await page.goto('https://mail.google.com');
 
-test('authenticate', async ({ page }) => {
-  // Navigate to Gmail login page
-  await page.goto('https://mail.google.com');
+  // // Fill in the email address and proceed
+  // await page.waitForSelector('input[type="email"]');
+  // await page.fill('input[type="email"]', 'testng46@gmail.com');
+  // await page.getByRole('button', { name: 'Next' }).click();
 
-  // Fill in the email address and proceed
-  await page.waitForSelector('input[type="email"]');
-  await page.fill('input[type="email"]', 'testng46@gmail.com');
-  await page.getByRole('button', { name: 'Next' }).click();
+  // // Wait for the password input field and fill in the password
+  // await page.waitForSelector('input[type="password"]');
+  // await page.fill('input[type="password"]', 'Test&1234');
+  // await page.getByRole('button', { name: 'Next' }).click();
+//  //   const currentURL = await page.url();
+//  //   expect(currentURL).toContain("inbox");
+//  await page.waitForURL("https://mail.google.com/mail/u/0/#inbox");
 
-  // Wait for the password input field and fill in the password
-  await page.waitForSelector('input[type="password"]');
-  await page.fill('input[type="password"]', 'Test&1234');
-  await page.getByRole('button', { name: 'Next' }).click();
- //   const currentURL = await page.url();
- //   expect(currentURL).toContain("inbox");
- await page.waitForURL("https://mail.google.com/mail/u/0/#inbox");
+//  //   Wait for login to complete
+//  await page.waitForLoadState('domcontentloaded');
+//  // await page.waitForNavigation("domcontentloaded");
 
- //   Wait for login to complete
- await page.waitForLoadState('domcontentloaded');
- // await page.waitForNavigation("domcontentloaded");
+//   // Save authentication state
+//   await page.context().storageState({ path: 'playwright/.auth/gmail-auth.json' });
+//   await page.waitForTimeout(2000);
+// });
 
-  // Save authentication state
-  await page.context().storageState({ path: 'playwright/.auth/gmail-auth.json' });
-  await page.waitForTimeout(2000);
+// tests/auth.setup.js
+const { test: setup } = require('@playwright/test');
 
+const authFile = 'playwright/.auth/user.json';
 
-
-
-//   await page.getByRole('button', { name: 'Next' }).click();
-//   await page.getByLabel('Enter your password').fill('Test&1234');
-//   await page.getByLabel('Enter your password').press('Enter');
-//   await page.goto('https://mail.google.com/mail/u/0/');
-//   await page.goto('https://mail.google.com/mail/u/0/#inbox');
-
-  
-  
-  
-  
-
+setup('authenticate', async ({ request }) => {
+  // Send authentication request to Gmail. Replace with your own.
+  await request.post('https://accounts.google.com/signin/v2/identifier', {
+    form: {
+      'identifier': 'testng46@gmail.com', // Replace with your Gmail email address
+      'password': 'Test&1234', // Replace with your Gmail password
+    }
+  });
+  await request.storageState({ path: authFile });
 });
+
--- t/gmail.spec.js
+++ t/gmail.spec.js
@@ -4,6 +4,18 @@ const { test, expect } = require('@playwright/test');
 test('Gmail login', async ({ page }) => {
 //   // Check if the user is logged in
 await page.goto('https://mail.google.com');
+
+await expect(page.getByRole('link', { name: 'Sign in' })).toBeVisible();
+await page.getByRole('link', { name: 'Sign in' }).click();
+
+await page.waitForSelector('input[type="email"]');
+await page.fill('input[type="email"]', 'testng46@gmail.com');
+await page.getByRole('button', { name: 'Next' }).click();
+// Wait for the password input field and fill in the password
+await page.waitForSelector('input[type="password"]');
+await page.fill('input[type="password"]', 'Test&1234');
+await page.getByRole('button', { name: 'Next' }).click();
+
 await expect (page.getByRole('button', { name: 'More labels' })).toBeVisible();
 await page.getByRole('button', { name: 'More labels' }).click();
 await expect(page.getByLabel('All Mail')).toBeVisible();
