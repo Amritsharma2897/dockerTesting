551dd29bc3819782058781a087fec0197b8c804e
amritsharma2897 <amrit.sharma@clanap.com>
2024-03-18 18:11:37 +0530

    updated config file

--- playwright.config.js
+++ playwright.config.js
@@ -36,7 +36,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    // baseURL: 'https://opensource-demo.orangehrmlive.com/',
+    baseURL: process.env.URL ? 'https://opensource-demo.orangehrmlive.com/' : 'https://opensource-demo.orangehrmlive.com/',
     screenshot: "on",
     headless: true,
     video: "on",
